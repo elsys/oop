c40ccf6af34bbb50f566471364f87366d4b80f91
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2019-11-05 22:58:01 +0200

    Add partial implementation of Rational

---
+++ materials/2019-2020/11v/rational/rational.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+
+class Rational {
+  long numerator;
+  long denominator;
+
+public:
+  class ZeroDenominatorError {};
+
+  Rational(long numerator, long denominator)
+    : numerator(numerator), denominator(denominator) {
+    if (denominator == 0) {
+      throw ZeroDenominatorError();
+    }
+  }
+
+  long get_numerator() const  {
+    return numerator;
+  }
+
+  long get_denominator() const {
+    return denominator;
+  }
+
+  Rational reciprocal() const {
+    return Rational(denominator, numerator);
+  }
+
+  Rational operator-() const {
+    return Rational(-numerator, denominator);
+  }
+};
+
+std::ostream& operator<<(std::ostream& os, Rational const& r) {
+  return os
+    << r.get_numerator()
+    << '/'
+    << r.get_denominator();
+}
+
+int main() {
+  try {
+    Rational r(1, 0);
+  } catch (Rational::ZeroDenominatorError) {
+    std::cout << "Zero denominator\n";
+  }
+
+  Rational const r(2, 3);
+  Rational const rr = r.reciprocal();
+
+  std::cout << rr << '\n'; // <=> operator<<(operator<<(std::cout, rr), '\n');
+  std::cout << -rr << '\n'; // <=> std::cout << rr.operator-() << '\n';
+
+  return 0;
+}
