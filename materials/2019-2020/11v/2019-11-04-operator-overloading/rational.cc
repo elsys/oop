7f97dfcf7676227263e871f6c629276d100dfcf3
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2019-11-10 23:59:48 +0200

    Overload arithmetic operators and conversion to double

--- materials/2019-2020/11v/2019-11-04-operator-overloading/rational.cc
+++ materials/2019-2020/11v/2019-11-04-operator-overloading/rational.cc
@@ -1,8 +1,40 @@
+#include <cstdlib>
 #include <iostream>
 
 class Rational {
   long numerator;
   long denominator;
+
+  static long gcd(long x, long y) {
+    while (x != y) {
+      if (x > y) {
+        x -= y;
+      } else {
+        y -= x;
+      }
+    }
+
+    return x;
+  }
+
+  void reduce() {
+    if (numerator == 0) {
+      denominator = 1;
+    } else {
+      long divisor = gcd(std::labs(numerator), denominator);
+      numerator /= divisor;
+      denominator /= divisor;
+    }
+  }
+
+  void standardize() {
+    if (denominator < 0) {
+      numerator = -numerator;
+      denominator = -denominator;
+    }
+
+    reduce();
+  }
 
 public:
   class ZeroDenominatorError {};
@@ -12,6 +44,8 @@ public:
     if (denominator == 0) {
       throw ZeroDenominatorError();
     }
+
+    standardize();
   }
 
   long get_numerator() const  {
@@ -29,6 +63,50 @@ public:
   Rational operator-() const {
     return Rational(-get_numerator(), get_denominator());
   }
+
+  Rational operator+(Rational const& other) const {
+    long numerator =
+      get_numerator() * other.get_denominator() +
+      other.get_numerator() * get_denominator();
+    long denominator = get_denominator() * other.get_denominator();
+
+    return Rational(numerator, denominator);
+  }
+
+  Rational& operator+=(Rational const& other) {
+    return *this = *this + other;
+  }
+
+  Rational operator-(Rational const& other) const {
+    return *this + -other;
+  }
+
+  Rational& operator-=(Rational const& other) {
+    return *this = *this - other;
+  }
+
+  Rational operator*(Rational const& other) const {
+    long numerator = get_numerator() * other.get_numerator();
+    long denominator = get_denominator() * other.get_denominator();
+
+    return Rational(numerator, denominator);
+  }
+
+  Rational& operator*=(Rational const& other) {
+    return *this = *this * other;
+  }
+
+  Rational operator/(Rational const& other) const {
+    return *this * other.reciprocal();
+  }
+
+  Rational& operator/=(Rational const& other) {
+    return *this = *this / other;
+  }
+
+  operator double() const {
+    return (double)get_numerator() / get_denominator();
+  }
 };
 
 std::ostream& operator<<(std::ostream& os, Rational const& r) {
@@ -45,11 +123,19 @@ int main() {
     std::cout << "Zero denominator\n";
   }
 
-  Rational const r(2, 3);
-  Rational const rr = r.reciprocal();
+  Rational r(2, 3);
+  Rational rr = r.reciprocal();
 
   std::cout << rr << '\n'; // <=> operator<<(operator<<(std::cout, rr), '\n');
   std::cout << -rr << '\n'; // <=> std::cout << rr.operator-() << '\n';
+
+  std::cout << r + rr << '\n';
+  std::cout << r * rr << '\n';
+
+  (r += rr) += Rational(3, 4);
+  std::cout << r << '\n';
+
+  std::cout << (double)r << '\n';
 
   return 0;
 }
