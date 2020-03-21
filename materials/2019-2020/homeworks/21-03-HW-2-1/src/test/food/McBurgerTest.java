14067a70fe5d36c314d5c979c6837836fea660ea
Mario Benov <mario.ben@mail.bg>
2020-03-21 13:16:48 +0200

    add java hw 1

---
+++ materials/2019-2020/homeworks/21-03-HW-2-1/src/test/food/McBurgerTest.java
@@ -0,0 +1,22 @@
+package test.food;
+
+// import org.elsys.squirrels.food.McBurger;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class McBurgerTest {
+    @Test
+    void passWinter() {
+        // McBurger burger = new McBurger(20, 10);
+        // assertEquals(burger.getFoodScore(), 200);
+        // 
+        // burger.passWinter();
+        // assertEquals(burger.getFoodScore(), 80);
+        // assertEquals(burger.getAge(), 1);
+        //
+        // burger.passWinter();
+        // assertEquals(burger.getFoodScore(), 80);
+        // assertEquals(burger.getAge(), 2);
+    }
+}
---
+++ materials/2019-2020/homeworks/21-03-HW-2-1/src/test/food/NutsTest.java
@@ -0,0 +1,22 @@
+package test.food;
+
+// import org.elsys.squirrels.food.Nuts;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NutsTest {
+    @Test
+    void passWinter() {
+        // Nuts nuts = new Nuts(20, 10);
+        // assertEquals(nuts.getFoodScore(), 200);
+        //
+        // nuts.passWinter();
+        // assertEquals(nuts.getFoodScore(), 200);
+        // assertEquals(nuts.getAge(), 1);
+        //
+        // nuts.passWinter();
+        // assertEquals(nuts.getFoodScore(), 200);
+        // assertEquals(nuts.getAge(), 2);
+    }
+}
