4283c97e6f1f13e9d58a8149b1db70c8a6ef71e7
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2020-05-05 15:17:21 +0300

    Add lambdas exercise initial project code

---
+++ materials/2019-2020/11v/2020-05-05-lambda-functions-exercise/src/org/elsys/duzunov/YouTubeTrendsExplorer.java
@@ -0,0 +1,23 @@
+package org.elsys.duzunov;
+
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Scanner;
+
+public class YouTubeTrendsExplorer {
+    /**
+     * Loads the dataset from the given {@code dataInput} stream.
+     */
+    public YouTubeTrendsExplorer(InputStream dataInput) {
+        throw new UnsupportedOperationException("Method not yet implemented");
+    }
+
+    /**
+     * Returns all videos loaded from the dataset.
+     **/
+    public Collection<TrendingVideo> getTrendingVideos() {
+        return null;
+    }
+
+    // Other methods ...
+}
