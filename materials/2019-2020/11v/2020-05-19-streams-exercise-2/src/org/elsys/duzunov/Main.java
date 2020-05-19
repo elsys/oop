806e2fdc653cb60bc21ef1a2d6985489719a3604
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2020-05-19 17:28:37 +0300

    Add partial solution to Movies task

--- materials/2019-2020/11v/2020-05-19-streams-exercise-2/src/org/elsys/duzunov/Main.java
+++ materials/2019-2020/11v/2020-05-19-streams-exercise-2/src/org/elsys/duzunov/Main.java
@@ -1,6 +1,23 @@
 package org.elsys.duzunov;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            InputStream moviesInputStream = Files.newInputStream(
+                    Path.of("movies-mpaa.txt")
+            );
+            var explorer = new MoviesExplorer(moviesInputStream);
+
+            System.out.println(explorer.countMoviesReleasedInYear(2003));
+            System.out.println(explorer.findFirstMovieWithTitle("car"));
+            System.out.println(explorer.getAllActors());
+        } catch (IOException exception) {
+            System.out.println("Could not load videos dataset.");
+        }
     }
 }
--- materials/2019-2020/11v/2020-05-19-streams-exercise-2/src/org/elsys/duzunov/MoviesExplorer.java
+++ materials/2019-2020/11v/2020-05-19-streams-exercise-2/src/org/elsys/duzunov/MoviesExplorer.java
@@ -1,20 +1,85 @@
 package org.elsys.duzunov;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MoviesExplorer {
+    private final List<Movie> movies;
+
     /**
      * Loads the dataset from the given {@code dataInput} stream.
      */
     public MoviesExplorer(InputStream dataInput) {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        try (
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(dataInput))
+        ) {
+            movies = reader.lines()
+                    .map(Movie::createMovie)
+                    .collect(Collectors.toList());
+        } catch (IOException exception) {
+            throw new IllegalArgumentException(
+                    "Could not load dataset",
+                    exception
+            );
+        }
     }
 
     /**
      * Returns all the movies loaded from the dataset.
      **/
     public Collection<Movie> getMovies() {
-        return null;
+        return Collections.unmodifiableList(movies);
+    }
+
+    public long countMoviesReleasedInYear(int year) {
+//        return movies.stream()
+//                .filter(movie -> movie.getYear() == year)
+//                .reduce(
+//                        0,
+//                        (count, movie) -> count + 1,
+//                        Integer::sum
+//                );
+
+//        return movies.stream()
+//                .filter(movie -> movie.getYear() == year)
+//                .mapToLong(movie -> 1L)
+//                .sum();
+
+        return movies.stream()
+                .filter(movie -> movie.getYear() == year)
+                .count();
+    }
+
+    public Movie findFirstMovieWithTitle(String title) {
+        return movies.stream()
+                .filter(movie -> movie.getTitle().contains(title))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public Collection<Actor> getAllActors() {
+        return movies.stream()
+                .flatMap(movie -> movie.getActors().stream())
+                .collect(Collectors.toSet());
+    }
+
+    public int getFirstYear() {
+//        return movies.stream()
+//                .min(Comparator.comparingInt(Movie::getYear))
+//                .get()
+//                .getYear();
+
+        return movies.stream()
+                .map(Movie::getYear)
+                .min(Comparator.comparingInt(year -> year))
+                .get();
     }
 }
