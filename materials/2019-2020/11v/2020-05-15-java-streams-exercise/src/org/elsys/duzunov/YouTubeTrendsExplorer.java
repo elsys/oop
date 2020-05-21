0479c26e8c1f1297c03f59fa5ee661a2bc876800
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2020-05-21 16:40:22 +0300

    Add YouTube exercise solution with Stream API

--- materials/2019-2020/11v/2020-05-15-java-streams-exercise/src/org/elsys/duzunov/YouTubeTrendsExplorer.java
+++ materials/2019-2020/11v/2020-05-15-java-streams-exercise/src/org/elsys/duzunov/YouTubeTrendsExplorer.java
@@ -4,19 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiFunction;
-import java.util.function.BiPredicate;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class YouTubeTrendsExplorer {
@@ -50,129 +42,62 @@ public class YouTubeTrendsExplorer {
     }
 
     public String findIdOfLeastLikedVideo() {
-        return findMinimum(
-                trendingVideos,
-                Comparator.comparingLong(TrendingVideo::getLikes)
-        ).getId();
+        return trendingVideos.stream()
+                .min(Comparator.comparingLong(TrendingVideo::getLikes))
+                .get()
+                .getId();
     }
 
     public String findIdOfMostLikedLeastDislikedVideo() {
-        return findMaximum(
-                trendingVideos,
-                Comparator.comparingLong(
-                        video -> video.getLikes() - video.getDislikes()
+        return trendingVideos.stream()
+                .max(
+                        Comparator.comparingLong(
+                                video -> video.getLikes() - video.getDislikes()
+                        )
                 )
-        ).getId();
+                .get()
+                .getId();
     }
 
     public List<String> findDistinctTitlesOfTop3VideosByViews() {
-        Comparator<TrendingVideo> viewsComparator =
-                Comparator.comparingLong(TrendingVideo::getViews).reversed();
-        trendingVideos.sort(viewsComparator);
-        HashSet<TrendingVideo> uniqueVideos = new HashSet<>(trendingVideos);
-        ArrayList<TrendingVideo> sortedVideos = new ArrayList<>(uniqueVideos);
-        sortedVideos.sort(viewsComparator);
-
-        List<TrendingVideo> top3 = take(sortedVideos, 3);
-        return map(top3, TrendingVideo::getTitle);
+        return trendingVideos.stream()
+                .sorted(
+                        Comparator.comparingLong(TrendingVideo::getViews)
+                                .reversed()
+                )
+                .distinct()
+                .map(TrendingVideo::getTitle)
+                .limit(3)
+                .collect(Collectors.toList());
     }
 
     public String findIdOfMostTaggedVideo() {
-        return findMaximum(
-                trendingVideos,
-                Comparator.comparingInt(video -> video.getTags().size())
-        ).getId();
+        return trendingVideos.stream()
+                .max(Comparator.comparingInt(video -> video.getTags().size()))
+                .get()
+                .getId();
     }
 
     public String findTitleOfFirstVideoTrendingBefore100KViews() {
-        var lessThan100KViews =
-                filter(trendingVideos, video -> video.getViews() < 100_000);
-
-        return findMinimum(
-                lessThan100KViews,
-                Comparator.comparing(TrendingVideo::getPublishDate)
-        ).getTitle();
+        return trendingVideos.stream()
+                .filter(video -> video.getViews() < 100_000)
+                .min(Comparator.comparing(TrendingVideo::getPublishDate))
+                .get()
+                .getTitle();
     }
 
     public String findIdOfMostTrendingVideo() {
-        HashMap<String, Integer> trendingVideosCountById =
-                groupBy(
-                        trendingVideos,
-                        TrendingVideo::getId,
-                        video -> 1,
-                        Integer::sum
+        Map<String, Long> trendingVideosCountById = trendingVideos.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                TrendingVideo::getId,
+                                Collectors.counting()
+                        )
                 );
 
-        return findMaximum(
-                trendingVideosCountById.entrySet(),
-                Comparator.comparingInt(Map.Entry::getValue)
-        ).getKey();
-    }
-
-    private <T, K, V> HashMap<K, V> groupBy(Iterable<T> elements,
-                                            Function<T, K> keyMapper,
-                                            Function<T, V> valueMapper,
-                                            BiFunction<V, V, V> combiner) {
-        HashMap<K, V> groups = new HashMap<>();
-        for (T element : elements) {
-            K key = keyMapper.apply(element);
-            V value = valueMapper.apply(element);
-            groups.merge(key, value, combiner);
-        }
-        return groups;
-    }
-
-    private <T> List<T> filter(Iterable<T> elements, Predicate<T> tester) {
-        ArrayList<T> filtered = new ArrayList<>();
-        for (T element : elements) {
-            if (tester.test(element)) {
-                filtered.add(element);
-            }
-        }
-        return filtered;
-    }
-
-    private <T, R> List<R> map(Iterable<T> elements, Function<T, R> mapper) {
-        ArrayList<R> mapped = new ArrayList<>();
-        for (T element : elements) {
-            mapped.add(mapper.apply(element));
-        }
-        return mapped;
-    }
-
-    private <T> List<T> take(Iterable<T> elements, int n) {
-        ArrayList<T> firstN = new ArrayList<>();
-        Iterator<T> iterator = elements.iterator();
-        for (int i = 0; iterator.hasNext() && i < n; ++i) {
-            firstN.add(iterator.next());
-        }
-        return firstN;
-    }
-
-    private <T> T findMinimum(Iterable<T> elements, Comparator<T> comparator) {
-        return findExtremum(
-                elements,
-                (element1, element2) ->
-                        comparator.compare(element1, element2) < 0
-        );
-    }
-
-    private <T> T findMaximum(Iterable<T> elements, Comparator<T> comparator) {
-        return findMinimum(elements, comparator.reversed());
-    }
-
-    private static <T> T findExtremum(
-            Iterable<T> elements,
-            BiPredicate<T, T> tester
-    ) {
-        Iterator<T> iterator = elements.iterator();
-        T extrumum = iterator.next();
-        while (iterator.hasNext()) {
-            T currentElement = iterator.next();
-            if (tester.test(currentElement, extrumum)) {
-                extrumum = currentElement;
-            }
-        }
-        return extrumum;
+        return trendingVideosCountById.entrySet().stream()
+                .max(Comparator.comparingLong(Map.Entry::getValue))
+                .get()
+                .getKey();
     }
 }
