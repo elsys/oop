2b68d1cf79506ba0ff4cbc11588e56404f5e2763
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2020-05-23 14:18:28 +0300

    Add mountains.txt reading

--- materials/2019-2020/11v/2020-05-21-java-streams-exercise-3/src/org/elsys/duzunov/Main.java
+++ materials/2019-2020/11v/2020-05-21-java-streams-exercise-3/src/org/elsys/duzunov/Main.java
@@ -1,6 +1,19 @@
 package org.elsys.duzunov;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            InputStream peaksInputStream = Files.newInputStream(
+                    Path.of("mountains.txt")
+            );
+            var peakStats = new PeakStats(peaksInputStream);
+        } catch (IOException exception) {
+            System.out.println("Could not load peaks dataset.");
+        }
     }
 }
