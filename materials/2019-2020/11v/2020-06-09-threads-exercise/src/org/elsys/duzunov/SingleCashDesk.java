7acab4a918c35c4f1c1a7b2bdc87b7651379422d
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2020-06-09 11:45:04 +0300

    Remove unnecessary constructor

--- materials/2019-2020/11v/2020-06-09-threads-exercise/src/org/elsys/duzunov/SingleCashDesk.java
+++ materials/2019-2020/11v/2020-06-09-threads-exercise/src/org/elsys/duzunov/SingleCashDesk.java
@@ -3,9 +3,6 @@ package org.elsys.duzunov;
 public class SingleCashDesk implements CashDesk {
     private static final int MAX_CASH = 100;
 
-    public SingleCashDesk() {
-    }
-
     @Override
     public void serveCustomer(Customer customer) {
         throw new UnsupportedOperationException("Please implement this method");
