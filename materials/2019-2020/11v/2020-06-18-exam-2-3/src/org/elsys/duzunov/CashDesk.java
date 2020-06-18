4f0ee472528f987e6ec784dde989aba7e81216fc
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2020-06-18 14:07:23 +0300

    Add third Java exam task and initial project files

---
+++ materials/2019-2020/11v/2020-06-18-exam-2-3/src/org/elsys/duzunov/CashDesk.java
@@ -0,0 +1,44 @@
+package org.elsys.duzunov;
+
+public class CashDesk {
+    private static final int MAX_CARDS = 1000;
+    private static final int CARD_PRICE = 50;
+    private static final int DEPOSIT = 1;
+
+    /**
+     * Gives a card to a snowboarder. The snowboarder pays CARD_PRICE + DEPOSIT.
+     */
+    public void buyCard(Snowboarder snowboarder) {
+    }
+
+    /**
+     * Returns the deposit to a snowboarder.
+     */
+    public void returnCard(Snowboarder snowboarder) {
+    }
+
+    /**
+     * Returns the cash amount currently contained in this cash desk.
+     */
+    public int getAmount() {
+    }
+
+    /**
+     * Returns the maximum number of available cards in this cash desk.
+     */
+    public int getMaxAvailableCardsCount() {
+    }
+
+    /**
+     * Returns the number of sold cards.
+     */
+    public int getSoldCardsCount() {
+    }
+
+    /**
+     * Returns the number of available cards currently contained in this cash
+     * desk.
+     */
+    public int getAvailableCardsCount() {
+    }
+}
---
+++ materials/2019-2020/11v/2020-06-18-exam-2-3/src/org/elsys/duzunov/Snowboarder.java
@@ -0,0 +1,12 @@
+package org.elsys.duzunov;
+
+public class Snowboarder extends Thread {
+    private final CashDesk cashDesk;
+
+    public Snowboarder(String name, CashDesk cashDesk) {
+    }
+
+    @Override
+    public void run() {
+    }
+}
